9ebe11b2d0b8010adf1d3f47771d8da5e3e61c6b
Danielle Martinoli <daniechris5@gmail.com>
2019-05-08 17:01:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
   array.join
-  array.join(' and ')
-  array.join(' and ')
-  array.join(',')
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
